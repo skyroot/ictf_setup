8654319635e5edefa9242255b8bb97bd910ad700
Calvin <tacalvin123@gmail.com>
2017-04-17 11:42:05 -0700

    Bug fixes

--- Vagrantfile
+++ Vagrantfile
@@ -3,14 +3,16 @@
 
 Vagrant.configure(2) do |config|
     #'internal boxes'
+    config.env.enable
     config.vm.define("ubuntu_box") do |ubuntu_config|
         ubuntu_config.vm.box = "ubuntu/xenial64"
-        ubuntu_config.vm.provision "shell" ,path: "./setup.sh"do |shell|
-            shell.args =  "#{ENV['password_root']} #{ENV['password_ctf']}" 
+        ubuntu_config.vm.provision "shell"do |shell|
+	    shell.path = "./setup.sh"
+            shell.args =  "#{ENV['password_root']} #{ENV['password_ctf']}"
         end
     end
-    config.vm.provider "virtualbox" do |v|
-        v.cpus = 3
-        v.memory = 2056
-    end
+#    config.vm.provider "virtualbox" do |v|
+#        v.cpus = 3
+#        v.memory = 2056
+#    end
 end
--- setup.sh
+++ setup.sh
@@ -1,12 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 sudo apt-get --assume-yes update
 sudo apt-get --assume-yes install build-essential
 sudo apt-get --assume-yes install virtualbox
 #database password for root user
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password '$1''
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password '$1''
+sudo debconf-set-selections <<< 'mysql-server-5.7 mysql-server/root_password password '$1''
+sudo debconf-set-selections <<< 'mysql-server-5.7 mysql-server/root_password_again password '$1''
 sudo apt-get --assume-yes install mysql-server
 sudo apt-get --assume-yes install libmysqlclient-dev
+
+#create ctf user
+mysql -uroot -p$1 -e "CREATE USER 'ctf'@localhost IDENTIFIED BY '$2';"
+mysql -uroot -p$1 -e "GRANT ALL PRIVILEGES ON * . * TO 'ctf'@localhost;"
+echo "Success"
+
+
 sudo apt-get --assume-yes install python-pip
 sudo apt-get --assume-yes install python-dev
 /usr/bin/yes | sudo pip install virtualenv
@@ -19,4 +26,3 @@ source ictf_env/bin/activate
 /usr/bin/yes | sudo pip install mysql-python
 #commandline arg for password
 sed -i '3s/.*/MYSQL_DATABASE_PASSWORD = \"'$2'\"/g' database/settings.py
-echo $1
